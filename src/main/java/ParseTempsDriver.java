d0be10cfa4c9b3a9149ff4b1d3db8ec87e1edb12
Dillon Sapp <87584441+klevvergirl@users.noreply.github.com>
2025-07-28 19:54:54 -0400

    Updated Operations to include PWI and LSA

--- src/main/java/ParseTempsDriver.java
+++ src/main/java/ParseTempsDriver.java
@@ -1,11 +1,18 @@
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static edu.odu.cs.cs417.TemperatureParser.CoreTempReading;
 import static edu.odu.cs.cs417.TemperatureParser.parseRawTemps;
+import static edu.odu.cs.cs417.Operations.PieceWise;
+import static edu.odu.cs.cs417.Operations.LeastSquares;
+import static edu.odu.cs.cs417.Operations.CubicSpline;
 
 /**
  * A simple command line test driver for TemperatureParser.
@@ -20,16 +27,21 @@ public class ParseTempsDriver {
     public static void main(String[] args)
     {
         BufferedReader tFileStream = null;
+        String filename = "";
 
         // Parse command line argument 1
         try {
             tFileStream = new BufferedReader(new FileReader(new File(args[0])));
+            Path path = Paths.get(args[0]);
+            filename = path.getFileName().toString();
         }
         catch (ArrayIndexOutOfBoundsException e) {
-            // TBW
+            System.out.println("A file path must be provided.");
+            System.exit(1);
         }
         catch (FileNotFoundException e) {
-            // TBW
+            System.out.println("Could not locate a file with the supplied file path.");
+            System.exit(1);
         }
 
         List<CoreTempReading> allTheTemps = parseRawTemps(tFileStream);
@@ -54,18 +66,88 @@ public class ParseTempsDriver {
             }
         }
 
-        //----------------------------------------------------------------------
-        // Output times alongside each core
-        //----------------------------------------------------------------------
+        //----------------------------------------------
+        // Output times alongside each core to console
+        // Create a new file for each core
+        // Write each 
+        //   -piece-wise interpolation, 
+        //   -least squares approximation, 
+        //   -cubic spline 
+        // to the file.
+        //----------------------------------------------
         System.out.println();
+
+        //------------------------------------------
+        // Declare variables to write to file
+        //------------------------------------------
+        StringBuilder stringbuilder = null;
+        FileWriter WriteToCore = null;
+        PieceWise PWbuilder = null;
+        LeastSquares LSbuilder = null;
+
         for (int coreIdx = 0; coreIdx < numberOfCores; ++coreIdx) {
+
             System.out.printf("Core # %2d%n", coreIdx);
+
+            //----------------------------------------------
+            // Create a new file and name it in the format
+            // {input file name}-core-0{core number}.txt
+            //----------------------------------------------
+            String moddedfilename = filename.replace(".txt", "-core-0" + coreIdx + ".txt" );
+            File core = new File(moddedfilename);
+            
+            try{
+                if (core.createNewFile()){
+                    System.out.println("File " + core.getName() + " created.");
+                }
+                else{
+                    System.out.println("File already exists.");
+                }
+            }
+            catch(IOException e){
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
+            //----------------------------------------------
+            // Print the times and readings to the console
+            // Write each equation to a string builder
+            //----------------------------------------------
+            try{
+                WriteToCore = new FileWriter(core); 
+                stringbuilder = new StringBuilder();  
+            }
+            catch(IOException e){
+                System.out.println("Failed to write to file.");
+                e.printStackTrace();
+            }
 
             for (int i = 0; i < times.length; ++i) {
                 System.out.printf("%8d -> %5.2f%n", times[i], coreReadings[coreIdx][i]);
+                
+                if(i != times.length - 1){
+                    PWbuilder = new PieceWise(times[i], times[i+1], coreReadings[coreIdx][i], coreReadings[coreIdx][i+1]);
+                    stringbuilder.append(PWbuilder.calculate());
+                }
             }
 
+            LSbuilder = new LeastSquares(times[0], times[times.length - 1], coreReadings[coreIdx][0], coreReadings[coreIdx][times.length - 1]);
+            stringbuilder.append(LSbuilder.calculate());
+
             System.out.println();
+
+            //----------------------------------------------------
+            // Write each equation to the file and close it
+            //----------------------------------------------------
+
+            try{
+                WriteToCore.write(stringbuilder.toString());
+                WriteToCore.close();
+            }
+            catch(IOException e){
+                System.out.println("Failed to close file.");
+                e.printStackTrace();  
+            }
         }
     }
 }
--- src/main/java/edu/odu/cs/cs417/Operations.java
+++ src/main/java/edu/odu/cs/cs417/Operations.java
@@ -1,10 +1,258 @@
 package edu.odu.cs.cs417;
 
-import java.io.BufferedReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
+import java.lang.Math;
 
 public class Operations
 {
+    public static class PieceWise{
+
+       private int currentTime;
+
+       private int nextTime;
+
+       private double currentReading;
+
+       private double nextReading;
+
+       private double multiplier;
+
+       private double adder;
+
+
+        /** 
+         * Set all variables to the provided values
+         * 
+         * @param curr The current reading's timestamp
+         * @param next The next reading's timestamp
+         * @param currR The current reading
+         * @param nextR The next reading
+         */
+        public PieceWise(int curr, int next, double currR, double nextR){
+            this.currentTime = curr;
+            this.nextTime = next;
+            this.currentReading = currR;
+            this.nextReading = nextR;
+        }
+
+        /**
+         * Calculate the values of adder and multiplier to build an equation. 
+         * Then return a string for the equation at the specified time interval
+         * in the format:
+         * 
+         * @return buildString()
+         */
+        public String calculate(){
+
+            multiplier = (nextReading - currentReading) / (nextTime - currentTime);
+            adder = currentReading - (multiplier * currentTime);
+
+            return buildString();
+        }
+
+        /**
+         * Build a string in the format:
+         * 
+         *       currentTime <= x <= nextTime; y = adder + multiplier(x); type
+         * 
+         * Where - 
+         *       * currentTime is the current reading's timestamp
+         *       * nextTime is the next reading's timestamp
+         *       * multiplier = (nextReading - currentReading) / (nextTime - currentTime);
+         *       * adder = currentReading - (multiplier * currentTime);
+         *       * type is 'interpolation'
+         * 
+         * @return bld.toString()
+         */
+        private String buildString(){
+
+            StringBuilder bld = new StringBuilder();
+            
+            bld.append(currentTime)
+               .append(" <= x <= ")
+               .append(nextTime).append("; y = ")
+               .append(String.format("%10.4f", adder))
+               .append(" + ")
+               .append(String.format("%10.4f", multiplier))
+               .append("x; interpolation \n");
+
+            return bld.toString();
+        }
+    }
+
+    public static class LeastSquares{
+
+        private int firstTime;
+
+        private int lastTime;
+
+        private double firstReading;
+
+        private double lastReading;
+
+        private double adder;
+
+        private double multiplier;
+
+
+        /** 
+        * Set all variables to the provided values
+        * 
+        * @param first The first reading's timestamp
+        * @param last The last reading's timestamp
+        * @param fRead The first reading
+        * @param lRead The last reading
+        */
+        public LeastSquares(int first, int last, double fRead, double lRead){
+            this.firstTime = first;
+            this.lastTime = last;
+            this.firstReading = fRead;
+            this.lastReading = lRead;
+        }
+
+        /**
+        * Calculate the values of adder and multiplier to build an equation. 
+        *
+        *@return The String built by buildString()
+        */
+        public String calculate(){
+
+            //-----------------------------------------------------
+            // Initialize a set of matrices to perform
+            // the XTX|XTY method for Least Squares Approximation
+            //-----------------------------------------------------
+            double[][] X = {
+                { 1 , firstTime },
+                { 1 , lastTime  }
+            };
+
+            double[][] XT = {
+                {     1   ,     1   },
+                {firstTime, lastTime}
+            };
+            double[] Y = 
+                { firstReading,
+                  lastReading  };
+
+            double[][] XTX = new double[2][2];
+            double[]   XTY = new double[2];
+            double[] Gaussed = new double[2];
+
+            //------------------------------------------------------------
+            //Matrix multiply XT and X and store the result in resultant
+            //------------------------------------------------------------
+            for(int i = 0; i < 2; i++){
+                for(int j = 0; j < 2; j++){
+                    for(int k = 0; k < 2; k++){
+                        XTX[i][j] += XT[i][k] * X[k][j];
+                    }
+                }
+            }
+
+            //------------------------------------------------------------
+            //Matrix multiply XT and Y and store the result in resultant
+            //------------------------------------------------------------
+            for(int i = 0; i < 2; i++){
+                for(int j = 0; j < 1; j++){
+                    for(int k = 0; k < 2; k++){
+                        XTY[i] += XT[i][k] * Y[k];
+                    }
+                }
+            }
+
+            //---------------------------------------------------------
+            //Perform Gaussian Elimination, take the resultant values
+            //and store them in adder and multiplier
+            //---------------------------------------------------------
+            Gaussed = GaussElim(XTX, XTY);
+
+            adder = Gaussed[0];
+            multiplier = Gaussed[1];
+
+            return buildString();
+        }
+
+        /**
+         * An algorithm for performing Gaussian Elimination with partial pivoting
+         * graciously provided by Robert Sedgewick and Kevin Wayne of Princeton University.
+         * 
+         * @param XTX A two dimensional array equivalent to the left hand side of an augmented matrix
+         * @param XTY A single dimension array equivalent to the right hand side of an augmented matrix
+         * 
+         * @return A single dimension array of the resultant values 
+         * equivalent to the right hand side of an augmented matrix after elimination has been performed.
+         */
+
+        public static double[] GaussElim(double[][] XTX, double[] XTY) {
+            int n = XTY.length;
+
+            for (int p = 0; p < n; p++) {
+
+                //------------------------------
+                // Find the pivot row and swap
+                //------------------------------
+                int max = p;
+                for (int i = p + 1; i < n; i++) {
+                    if (Math.abs(XTX[i][p]) > Math.abs(XTX[max][p])) {
+                        max = i;
+                    }
+                }
+
+                double[] temp = XTX[p]; XTX[p] = XTX[max]; XTX[max] = temp;
+                double   t    = XTY[p]; XTY[p] = XTY[max]; XTY[max] = t;
+
+                //---------------------------
+                // Pivot within XTX and XTY
+                //---------------------------
+                for (int i = p + 1; i < n; i++) {
+                    double alpha = XTX[i][p] / XTX[p][p];
+                    XTY[i] -= alpha * XTY[p];
+                    for (int j = p; j < n; j++) {
+                        XTX[i][j] -= alpha * XTX[p][j];
+                    }
+                }
+            }
+
+            //------------------------------
+            // Perform back substitution
+            //------------------------------
+            double[] resultant = new double[n];
+
+            for (int i = n - 1; i >= 0; i--) {
+                double sum = 0.0;
+                for (int j = i + 1; j < n; j++) {
+                    sum += XTX[i][j] * resultant[j];
+                }
+                resultant[i] = (XTY[i] - sum) / XTX[i][i];
+            }
+
+            return resultant;
+        }
+
+        /**
+        * Build an equation in the format:
+        * 
+        *@return firstTime <= x <= lastTime; y = adder + multiplier(x); type
+        * 
+        * Where - 
+        *       * firstTime is the first reading's timestamp
+        *       * lastTime is the last reading's timestamp
+        *       * multiplier = 
+        *       * adder = 
+        *       * type is 'least squares'
+        */
+        private String buildString(){
+
+            StringBuilder bld = new StringBuilder();
+
+            bld.append(firstTime)
+               .append(" <= x <= ")
+               .append(lastTime).append("; y = ")
+               .append(String.format("%10.4f", adder))
+               .append(" + ")
+               .append(String.format("%10.4f", multiplier))
+               .append("x; least squares \n");
+
+            return bld.toString();
+        }
+    }
 }
